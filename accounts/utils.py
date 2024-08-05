a1d0e0272cf56ba47232989f2ef95503a97ce550
abdul <idbsch2012@gmail.com>
2024-08-05 13:30:22 -0400

    completed email notification

--- accounts/utils.py
+++ accounts/utils.py
@@ -55,6 +55,13 @@ def send_verification_email(request, user, mail_subject, email_template):
 #     mail = EmailMessage(mail_subject, message, from_email, to=[to_email])
 #     mail.send()
 
-    
+#this function is going to handle sending notification to user when the vendor is approved or otherwise
+def send_notification(mail_subject, mail_template, context):
+    from_email = settings.DEFAULT_FROM_EMAIL
+    message = render_to_string(mail_template, context)
+    to_email = context['user'].email
+    mail = EmailMessage(mail_subject, message, from_email, to=[to_email])
+    mail.send()
+
     
         
--- vendors/models.py
+++ vendors/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User, UserProfile
+from accounts.utils import send_notification
 
 # Create your models here.
 
@@ -14,3 +15,28 @@ class Vendor(models.Model):
 
     def __str__(self):
         return self.vendor_name
+    
+    def save(self, *args, **kwargs):
+        # check if the vendor is created already or exist
+        if self.pk is not None:
+            #update it
+            orig = Vendor.objects.get(pk=self.pk)
+            if orig.is_approved != self.is_approved:
+
+                mail_template = 'accounts/emails/admin_approval_email.html'
+                context = {
+                    'user':self.user,
+                    'is_approved': self.is_approved,
+                }
+                if self.is_approved == True:
+                    #Send approval email
+                    mail_subject = "Application approved"                    
+                    send_notification(mail_subject, mail_template, context)
+                else:
+                    # send non approval email
+                    mail_subject = "Application rejected"
+                    send_notification(mail_subject, mail_template, context)
+
+
+        #the super function allows you to access the save method of the vendor class
+        return super(Vendor, self).save(*args, **kwargs)
