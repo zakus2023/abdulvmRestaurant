a656aa7a6aef5a923b902df279404dacf84d9884
abdul <idbsch2012@gmail.com>
2024-08-25 11:02:40 -0400

    db backup

--- abdulmvrestaurant/accounts/signals.py
+++ abdulmvrestaurant/accounts/signals.py
@@ -3,21 +3,21 @@ from django.dispatch import receiver
 from .models import UserProfile, User
 
 #creating the django signal receiver
-@receiver(post_save, sender=User)
-def post_save_create_profile_receiver(sender, instance, created, **kwargs):
-    #print(created)
-    if created:
-        #print('Create the user profile'). This cretaes the profile when the user is first created
-        UserProfile.objects.create(user=instance)
-    else:
-        try:
-            #print("User is updated"). This saves the profile when the user is updated
-            profile = UserProfile.objects.get(user=instance)
-            profile.save()
-        except:
-            #create the profile if not exist. This is when the user is updated but the profile does not exist
-            UserProfile.objects.create(user=instance)
-            print("User recreated")
+# @receiver(post_save, sender=User)
+# def post_save_create_profile_receiver(sender, instance, created, **kwargs):
+#     #print(created)
+#     if created:
+#         #print('Create the user profile'). This cretaes the profile when the user is first created
+#         UserProfile.objects.create(user=instance)
+#     else:
+#         try:
+#             #print("User is updated"). This saves the profile when the user is updated
+#             profile = UserProfile.objects.get(user=instance)
+#             profile.save()
+#         except:
+#             #create the profile if not exist. This is when the user is updated but the profile does not exist
+#             UserProfile.objects.create(user=instance)
+#             print("User recreated")
 
 
 # This will be triggerred just before the user is saved
